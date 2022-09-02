be8bff9cf76e9d6f25c0c8dcaf5ae4d2ba4d1d36
tudorturcu <sendtudor@gmail.com>
2022-09-02 19:41:46 +0300

    adds 2 functioning requests for the weather-app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,23 @@
 const request = require('postman-request')
 
-const url = 'http://api.weatherstack.com/current?access_key=aa0f28a1625bad0af2bf98857b2f70ef&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=aa0f28a1625bad0af2bf98857b2f70ef&query=37.8267,-122.4233&units=f'
 
-request(url, (error, response, body) => {
-  const data = JSON.parse(body)
-  console.log(data.current)
+request({url:url, json: true}, (error, response, body) => {
+  const description = body.current.weather_descriptions[0]
+  const temperature = body.current.temperature
+  const feelslike = body.current.feelslike
+
+  console.log(description + '. It is currently ' + temperature + ' Fahrenheit degrees out. It feels like ' + feelslike + ' Fahrenheit degrees out.')
+})
+
+const mapboxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidHVkb3J0dXJjdSIsImEiOiJjazhnOW9nNWUwMGc0M2VxcmVpMGw3OTV2In0.tQzBoqQu1X3_yXw9F4-V-g&limit=1'
+
+request({url: mapboxUrl, json: true}, (error, response, body) => {
+  const placeCenter = body.features[0].center
+  const latitude = placeCenter[1]
+  const longitude = placeCenter[0]
+
+  console.log('Latitude is: ', latitude)
+  console.log('Longitude is: ', longitude)
+
 })
