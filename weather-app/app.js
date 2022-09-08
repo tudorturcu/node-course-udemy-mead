e182168a12d2390a67406aac5c3058c2d94d2fe0
Tudor Turcu <sendtudor@gmail.com>
2022-09-08 17:58:15 +0300

    playing with callbacks. using callbacks with our geocoding function

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
 const request = require('postman-request')
+const geocode = require('./utils/geocode.js')
 
 // const url = 'http://api.weatherstack.com/current?access_key=aa0f28a1625bad0af2bf98857b2f70ef&query=37.8267,-122.4233&units=f'
 
@@ -27,27 +28,34 @@ const request = require('postman-request')
 
 // Geocoding
 
-const mapboxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidHVkb3J0dXJjdSIsImEiOiJjazhnOW9nNWUwMGc0M2VxcmVpMGw3OTV2In0.tQzBoqQu1X3_yXw9F4-V-g&limit=1'
+// const mapboxUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidHVkb3J0dXJjdSIsImEiOiJjazhnOW9nNWUwMGc0M2VxcmVpMGw3OTV2In0.tQzBoqQu1X3_yXw9F4-V-g&limit=1'
 
-request({url: mapboxUrl, json: true}, (error, response, body) => {
-  if (error)
-  {
-    console.log('Unable to reach Mapbox Geocoding API')
-  }
-  else
-  {
-    if (body.features.length === 0)
-    {
-      console.log('No geocoding results matching the specified location')
-    }
-    else
-    {
-      const placeCenter = body.features[0].center
-      const latitude = placeCenter[1]
-      const longitude = placeCenter[0]
+// request({url: mapboxUrl, json: true}, (error, response, body) => {
+//   if (error)
+//   {
+//     console.log('Unable to reach Mapbox Geocoding API')
+//   }
+//   else
+//   {
+//     if (body.features.length === 0)
+//     {
+//       console.log('No geocoding results matching the specified location')
+//     }
+//     else
+//     {
+//       const placeCenter = body.features[0].center
+//       const latitude = placeCenter[1]
+//       const longitude = placeCenter[0]
     
-      console.log('Latitude is: ', latitude)
-      console.log('Longitude is: ', longitude)
-    }
-  }
+//       console.log('Latitude is: ', latitude)
+//       console.log('Longitude is: ', longitude)
+//     }
+//   }
+// })
+
+
+
+geocode('Boston', (error, data) => {
+  console.log('Error', error)
+  console.log('Data', data)
 })
