eedd37ad68b92b9dfb73fd7e8e7c0d52274a55b8
Tudor Turcu <sendtudor@gmail.com>
2022-09-10 13:44:03 +0300

    learn express with routes. finished tinkering with http and https core node modules.

--- playground/6-raw-http.js
+++ playground/6-raw-http.js
@@ -3,6 +3,21 @@ const https = require('https')
 
 const url = 'http://api.weatherstack.com/current?access_key=aa0f28a1625bad0af2bf98857b2f70ef&query=46,23&units=f'
 
-http.request(url, (response) => {
+const request = http.request(url, (response) => {
+  let data = ''
+  
+  response.on('data', (chunk) => {
+    data = data + chunk.toString()
+  })
 
+  response.on('end', () => {
+    const body = JSON.parse(data)
+    console.log(body)
+  })
 })
+
+request.on('error', (error) => {
+  console.log('An error', error)
+})
+
+request.end()
